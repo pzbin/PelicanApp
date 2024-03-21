a03c9fba4b43eebb6ed290c09c56d4bb7bc021bf
pzbin <peter.zbin1@gmail.com>
2024-03-21 14:11:14 +0100

    feat: initial spring-boot app structure

---
+++ src/main/java/com/pelican/pelicanapi/PelicanApiApplication.java
@@ -0,0 +1,13 @@
+package com.pelican.pelicanapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PelicanApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PelicanApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pelican/pelicanapi/PelicanApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pelican.pelicanapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PelicanApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
